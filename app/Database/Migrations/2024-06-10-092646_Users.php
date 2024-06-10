f607b18ff71743dae4b052ea6695b51d916df248
Novandra Maulana <75214830+notfoundra@users.noreply.github.com>
2024-06-10 16:56:49 +0700

    Initial commit

---
+++ app/Database/Migrations/2024-06-10-092646_Users.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_user' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'nama' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'role' => [
+                'type' => 'ENUM',
+                'constraint' => ['packing', 'aksesoris'],
+                'default' => 'user',
+            ],
+        ]);
+        $this->forge->addKey('id_user', true);
+        $this->forge->createTable('user');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user');
+    }
+}
